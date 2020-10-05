45a09060aa1408fa76c119512c595a0dfb989394
Miguel Ángel González Santamarta <mgons@unileon.es>
2020-10-05 20:25:50 +0200

    factory of factory created this factory creates abstract factories

---
+++ pddl_dao/pddl_dao/pddl_dao_factory/pddl_dao_factory_facory.py
@@ -0,0 +1,18 @@
+from enum import Enum, auto
+from pddl_dao.pddl_dao_factory.pddl_dao_factories.mongoengine_pddl_dao_factory import Mongoengine_PDDL_DAO_Factory
+
+
+class PDDL_DAO_Families(Enum):
+    MONGOENGINE = auto()
+
+
+class PDDL_DAO_FactoryFactory:
+
+    def __init__(self):
+        self.pddl_dao_families = PDDL_DAO_Families
+        self.__pddl_dao_type_families = {
+            self.pddl_dao_families.MONGOENGINE: Mongoengine_PDDL_DAO_Factory
+        }
+
+    def create_pddl_dao_factory(self, family):
+        return self.__pddl_dao_type_families[family]()
