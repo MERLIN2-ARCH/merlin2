b6842b846750090320c03bd78f3e6a391de99d6e
Miguel Ángel González Santamarta <mgons@unileon.es>
2020-10-14 21:52:45 +0200

    doc added

--- pddl_dao/pddl_dao/mongoengine_pddl_dao/mongoengine_connector.py
+++ pddl_dao/pddl_dao/mongoengine_pddl_dao/mongoengine_connector.py
@@ -1,17 +1,33 @@
 
+""" Mongoengine Connector """
+
 from mongoengine import connect, disconnect
 
 
 class MongoengineConnector:
+    """ Mongoengine Connector Class """
+
     def __init__(self, uri: str = "mongodb://localhost:27017/merlin2"):
         self._uri = uri
         self.connection = None
 
     def connect(self) -> bool:
+        """ connect to mongo db
+
+        Returns:
+            bool: succeed
+        """
+
         disconnect()
         self.connection = connect(host=self._uri)
         return True
 
     def close(self) -> bool:
+        """ close connection
+
+        Returns:
+            bool: succeed
+        """
+
         self.connection.close()
         return True
