e8044aa576ef41bba87ea625cd061d0b15957c6a
Miguel Ángel González Santamarta <mgons@unileon.es>
2020-10-17 22:07:13 +0200

    planners added to share

--- merlin2_plan_sys/merlin2_planner/setup.py
+++ merlin2_plan_sys/merlin2_planner/setup.py
@@ -1,4 +1,7 @@
 from setuptools import setup
+import os
+from glob import glob
+
 
 package_name = 'merlin2_planner'
 
@@ -10,6 +13,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'planners'), glob('planners/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
