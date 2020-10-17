e6d29646ac757e30414ea3363e0836cdd97776b9
Miguel Ángel González Santamarta <mgons@unileon.es>
2020-10-18 00:47:45 +0200

    merlin2 plan sus pkg created launch file for the plan sys

---
+++ merlin2_plan_sys/merlin2_plan_sys/launch/merlin2_plan_sys_launch.py
@@ -0,0 +1,47 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable
+import ament_index_python
+
+
+def generate_launch_description():
+    namespace = 'merlin2'
+
+    stdout_linebuf_envvar = SetEnvironmentVariable(
+        'RCUTILS_CONSOLE_STDOUT_LINE_BUFFERED', '1')
+
+    #
+    # NODES
+    #
+
+    pddl_generator_node_cmd = Node(
+        package='merlin2_pddl_generator',
+        executable='pddl_generator_node',
+        name='pddl_generator_node',
+        namespace=namespace,
+    )
+
+    planner_node_cmd = Node(
+        package='merlin2_planner',
+        executable='planner_node',
+        name='planner_node',
+        namespace=namespace,
+    )
+
+    executor_cmd = Node(
+        package="merlin2_executor",
+        executable='executor_node',
+        name='executor_node',
+        namespace=namespace,
+    )
+
+    ld = LaunchDescription()
+
+    ld.add_action(stdout_linebuf_envvar)
+
+    ld.add_action(pddl_generator_node_cmd)
+    ld.add_action(planner_node_cmd)
+    ld.add_action(executor_cmd)
+
+    return ld
