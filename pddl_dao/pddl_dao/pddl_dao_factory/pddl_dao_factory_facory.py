8ec9a3f17d2f5f606eb7d12a56f05857940ce078
Miguel Ángel González Santamarta <mgons@unileon.es>
2020-10-18 15:20:39 +0200

    kwargs added

--- pddl_dao/pddl_dao/pddl_dao_factory/pddl_dao_factory_facory.py
+++ pddl_dao/pddl_dao/pddl_dao_factory/pddl_dao_factory_facory.py
@@ -17,7 +17,7 @@ class PddlDaoFactoryFactory:
             self.pddl_dao_families.MONGOENGINE: MongoenginePddlDaoFactory
         }
 
-    def create_pddl_dao_factory(self, family: int) -> PddlDaoFactory:
+    def create_pddl_dao_factory(self, family: int, **kwargs) -> PddlDaoFactory:
         """ create a pddl dao factory of a given family
 
         Args:
@@ -27,4 +27,13 @@ class PddlDaoFactoryFactory:
             PddlDaoFactory: pddl dao factory
         """
 
-        return self.__families_to_factory[family]()
+        args_dict = {}
+
+        pddl_dao_factory = self.__families_to_factory[family]
+        init_args = list(pddl_dao_factory.__init__.__code__.co_varnames)
+
+        for key, value in kwargs.items():
+            if key in init_args:
+                args_dict[key] = value
+
+        return pddl_dao_factory(**args_dict)
