09abb8dd4324c62c711dd574834937be8fbde6ca
Miguel Ángel González Santamarta <mgons@unileon.es>
2020-10-23 15:41:43 +0200

    dto msg parsers created

---
+++ merlin2_knowledge_base/merlin2_knowledge_base/merlin2_knowledge_base/merlin2_knowled_base_parser/dto_msg_parser.py
@@ -0,0 +1,149 @@
+
+""" Dto Msg Parser """
+
+from merlin2_knowledge_base_interfaces.msg import PddlType
+from merlin2_knowledge_base_interfaces.msg import PddlObject
+from merlin2_knowledge_base_interfaces.msg import PddlPredicate
+from merlin2_knowledge_base_interfaces.msg import PddlProposition
+from merlin2_knowledge_base_interfaces.msg import PddlConditionEffect
+from merlin2_knowledge_base_interfaces.msg import PddlAction
+
+from pddl_dao.pddl_dto.pddl_type_dto import PddlTypeDto
+from pddl_dao.pddl_dto.pddl_object_dto import PddlObjectDto
+from pddl_dao.pddl_dto.pddl_predicate_dto import PddlPredicateDto
+from pddl_dao.pddl_dto.pddl_proposition_dto import PddlPropositionDto
+from pddl_dao.pddl_dto.pddl_condition_efect_dto import PddlConditionEffectDto
+from pddl_dao.pddl_dto.pddl_action_dto import PddlActionDto
+
+
+class DtoMsgParser:
+    """ Dto Msg Parser Class """
+
+    def type_dto_to_msg(self, pddl_type_dto: PddlTypeDto) -> PddlType:
+        """ convert a PddlTypeDto into a PddlType msg
+
+        Args:
+            pddl_type_dto (PddlTypeDto): PddlTypeDto
+
+        Returns:
+            PddlType: PddlType msg
+        """
+
+        msg = PddlType()
+
+        msg.type_name = pddl_type_dto.get_type_name()
+
+        return msg
+
+    def object_dto_to_msg(self, pddl_object_dto: PddlObjectDto) -> PddlObject:
+        """ convert a PddlObjectDto into a PddlObject msg
+
+        Args:
+            pddl_object_dto (PddlObjectDto): PddlObjectDto
+
+        Returns:
+            PddlObject: PddlObject msg
+        """
+
+        msg = PddlObject()
+
+        msg.pddl_type = self.type_dto_to_msg(pddl_object_dto.get_pddl_type())
+        msg.object_name = pddl_object_dto.get_object_name()
+
+        return msg
+
+    def predicate_dto_to_msg(self, pddl_predicate_dto: PddlPredicateDto) -> PddlPredicate:
+        """ convert a PddlPredicateDto into a PddlPredicate msg
+
+        Args:
+            pddl_predicate_dto (PddlPredicateDto): PddlPredicateDto
+
+        Returns:
+            PddlPredicate: PddlPredicate msg
+        """
+
+        msg = PddlPredicate()
+
+        msg.predicate_name = pddl_predicate_dto.get_predicate_name()
+
+        msg.pddl_types = []
+        for pddl_type_dto in pddl_predicate_dto.get_pddl_types_list():
+            msg.pddl_types.append(self.type_dto_to_msg(pddl_type_dto))
+
+        return msg
+
+    def proposition_dto_to_msg(self, pddl_proposition_dto: PddlPropositionDto) -> PddlProposition:
+        """ convert a PddlPropositionDto into a PddlProposition msg
+
+        Args:
+            pddl_proposition_dto (PddlPropositionDto): PddlPropositionDto
+
+        Returns:
+            PddlProposition: PddlProposition msg
+        """
+
+        msg = PddlProposition()
+
+        msg.pddl_predicate = self.predicate_dto_to_msg(
+            pddl_proposition_dto.get_pddl_predicate())
+
+        msg.is_goal = pddl_proposition_dto.get_is_goal()
+
+        msg.pddl_objects = []
+        for pddl_object_dto in pddl_proposition_dto.get_pddl_objects_list():
+            msg.pddl_objects.append(self.type_dto_to_msg(pddl_object_dto))
+
+        return msg
+
+    def condition_effect_dto_to_msg(self,
+                                    pddl_condition_efect_dto:
+                                        PddlConditionEffectDto) -> PddlConditionEffect:
+        """ convert a PddlConditionEffectDto into a PddlConditionEffect msg
+
+        Args:
+            pddl_condition_efect_dto (PddlConditionEffectDto): PddlConditionEffectDto
+
+        Returns:
+            PddlConditionEffect: PddlConditionEffect msg
+        """
+
+        msg = PddlConditionEffect()
+        msg.pddl_proposition = self.proposition_dto_to_msg(
+            pddl_condition_efect_dto)
+
+        msg.time = pddl_condition_efect_dto.get_time
+        msg.is_negative = pddl_condition_efect_dto.get_is_negative()
+
+        return msg
+
+    def action_dto_to_msg(self, pddl_action_dto: PddlActionDto) -> PddlAction:
+        """ convert a PddlActionDto into a PddlAction msg
+
+        Args:
+            pddl_action_dto (PddlActionDto): PddlActionDto
+
+        Returns:
+            PddlAction: PddlAction msg
+        """
+
+        msg = PddlAction()
+
+        msg.action_name = pddl_action_dto.get_action_name()
+        msg.duration = pddl_action_dto.get_duration()
+        msg.durative = pddl_action_dto.get_durative()
+
+        msg.pddl_parameters = []
+        for pddl_object_dto in pddl_action_dto.get_parameters_list():
+            msg.pddl_parameters.append(self.type_dto_to_msg(pddl_object_dto))
+
+        msg.pddl_coditions = []
+        for pddl_condition_dto in pddl_action_dto.get_conditions_list():
+            msg.pddl_coditions.append(
+                self.condition_effect_dto_to_msg(pddl_condition_dto))
+
+        msg.pddl_effects = []
+        for pddl_effect_dto in pddl_action_dto.get_effects_list():
+            msg.pddl_effects.append(
+                self.condition_effect_dto_to_msg(pddl_effect_dto))
+
+        return msg
---
+++ merlin2_knowledge_base/merlin2_knowledge_base/merlin2_knowledge_base/merlin2_knowled_base_parser/msg_dto_parser.py
@@ -0,0 +1,160 @@
+
+""" Msg Dto Parser """
+
+from merlin2_knowledge_base_interfaces.msg import PddlType
+from merlin2_knowledge_base_interfaces.msg import PddlObject
+from merlin2_knowledge_base_interfaces.msg import PddlPredicate
+from merlin2_knowledge_base_interfaces.msg import PddlProposition
+from merlin2_knowledge_base_interfaces.msg import PddlConditionEffect
+from merlin2_knowledge_base_interfaces.msg import PddlAction
+
+from pddl_dao.pddl_dto.pddl_type_dto import PddlTypeDto
+from pddl_dao.pddl_dto.pddl_object_dto import PddlObjectDto
+from pddl_dao.pddl_dto.pddl_predicate_dto import PddlPredicateDto
+from pddl_dao.pddl_dto.pddl_proposition_dto import PddlPropositionDto
+from pddl_dao.pddl_dto.pddl_condition_efect_dto import PddlConditionEffectDto
+from pddl_dao.pddl_dto.pddl_action_dto import PddlActionDto
+
+
+class MsgDtoParser:
+    """ Msg Dto Parser Class """
+
+    def type_msg_to_dto(self, pddl_type_msg: PddlType) -> PddlTypeDto:
+        """ convert a PddlType msg into a PddlTypeDto
+
+        Args:
+            pddl_type_msg (PddlType): PddlType msg
+
+        Returns:
+            PddlTypeDto: PddlTypeDto
+        """
+
+        dto = PddlTypeDto(pddl_type_msg.type_name)
+
+        return dto
+
+    def object_msg_to_dto(self, pddl_object_msg: PddlObject) -> PddlObjectDto:
+        """ convert a PddlObject msg into a PddlObjectDto
+
+        Args:
+            pddl_object_msg (PddlObject): PddlObject msg
+
+        Returns:
+            PddlObjectDto: PddlObjectDto
+        """
+
+        dto = PddlObjectDto(
+            self.type_msg_to_dto(pddl_object_msg.pddl_type),
+            pddl_object_msg.object_name
+        )
+
+        return dto
+
+    def predicate_msg_to_dto(self, pddl_predicate_msg: PddlPredicate) -> PddlPredicateDto:
+        """ convert a PddlPredicate msg into a PddlPredicateDto
+
+        Args:
+            pddl_predicate_msg (PddlPredicate): PddlPredicate msg
+
+        Returns:
+            PddlPredicateDto: PddlPredicateDto
+        """
+
+        dto = PddlPredicateDto(pddl_predicate_msg.predicate_name)
+
+        pddl_types_list = []
+
+        for pddl_type_msg in pddl_predicate_msg.pddl_types:
+            pddl_types_list.append(self.type_msg_to_dto(pddl_type_msg))
+
+        dto.set_pddl_types_list(pddl_types_list)
+
+        return dto
+
+    def proposition_msg_to_dto(self, pddl_proposition_msg: PddlProposition) -> PddlPropositionDto:
+        """ convert a PddlProposition msg into a PddlPropositionDto
+
+        Args:
+            pddl_proposition_msg (PddlProposition): PddlProposition msg
+
+        Returns:
+            PddlPropositionDto: PddlPropositionDto
+        """
+
+        dto = PddlPropositionDto(
+            self.predicate_msg_to_dto(pddl_proposition_msg.pddl_predicate)
+        )
+
+        dto.set_is_goal(pddl_proposition_msg.is_goal)
+
+        pddl_objects_list = []
+
+        for pddl_object_msg in pddl_proposition_msg.pddl_objects:
+            pddl_objects_list.append(self.type_msg_to_dto(pddl_object_msg))
+
+        dto.set_pddl_objects_list(pddl_objects_list)
+
+        return dto
+
+    def condition_effect_msg_to_dto(self,
+                                    pddl_condition_efect_msg:
+                                        PddlConditionEffect) -> PddlConditionEffectDto:
+        """ convert a PddlConditionEffect msg into a PddlConditionEffectDto
+
+        Args:
+            pddl_condition_efect_msg (PddlConditionEffect): PddlConditionEffect msg
+
+        Returns:
+            PddlConditionEffectDto: PddlConditionEffectDto
+        """
+
+        dto = PddlConditionEffectDto(
+            self.predicate_msg_to_dto(
+                pddl_condition_efect_msg.pddl_proposition.pddl_predicate)
+        )
+
+        dto.set_is_negative(pddl_condition_efect_msg.is_negative)
+        dto.set_time(pddl_condition_efect_msg.time)
+
+        pddl_objects_list = []
+
+        for pddl_object_msg in pddl_condition_efect_msg.pddl_proposition.pddl_objects:
+            pddl_objects_list.append(self.type_msg_to_dto(pddl_object_msg))
+
+        dto.set_pddl_objects_list(pddl_objects_list)
+
+        return dto
+
+    def action_msg_to_dto(self, pddl_action_msg: PddlAction) -> PddlActionDto:
+        """ convert a PddlAction msg into a PddlActionDto
+
+        Args:
+            pddl_action_msg (PddlAction): PddlAction msg
+
+        Returns:
+            PddlActionDto: PddlActionDto
+        """
+
+        dto = PddlActionDto(pddl_action_msg.action_name)
+
+        dto.set_duration(pddl_action_msg.duration)
+        dto.set_durative(pddl_action_msg.durative)
+
+        pddl_parameters_list = []
+        for pddl_object_msg in pddl_action_msg.pddl_parameters:
+            pddl_parameters_list.append(self.type_msg_to_dto(pddl_object_msg))
+        dto.set_parameters_list(pddl_parameters_list)
+
+        pddl_coditions_list = []
+        for pddl_condition_msg in pddl_action_msg.pddl_coditions:
+            pddl_coditions_list.append(
+                self.condition_effect_msg_to_dto(pddl_condition_msg))
+        dto.set_conditions_list(pddl_coditions_list)
+
+        pddl_effect_list = []
+        for pddl_effect_msg in pddl_action_msg.pddl_effects:
+            pddl_effect_list.append(
+                self.condition_effect_msg_to_dto(pddl_effect_msg))
+        dto.set_effects_list(pddl_coditions_list)
+
+        return dto
