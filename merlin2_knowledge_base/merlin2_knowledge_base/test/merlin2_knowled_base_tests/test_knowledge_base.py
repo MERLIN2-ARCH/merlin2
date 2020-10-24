8f426b8a0132becea88f298cc75c39b552b09190
Miguel Ángel González Santamarta <mgons@unileon.es>
2020-10-24 11:56:08 +0200

    knowledge_nase fixed

--- merlin2_knowledge_base/merlin2_knowledge_base/test/merlin2_knowled_base_tests/test_knowledge_base.py
+++ merlin2_knowledge_base/merlin2_knowledge_base/test/merlin2_knowled_base_tests/test_knowledge_base.py
@@ -14,7 +14,7 @@ class TestDtoMsgParser(unittest.TestCase):
 
     def setUp(self):
 
-        self.knowledge_nase = Merlin2KnowledgeBase()
+        self.knowledge_base = Merlin2KnowledgeBase()
 
         # types
         self.robot_type = PddlTypeDto("robot")
@@ -61,126 +61,135 @@ class TestDtoMsgParser(unittest.TestCase):
             "navigation", [r, s, d], [condition_1], [effect_1, effect_2])
 
     def tearDown(self):
-        self.knowledge_nase.delete_all_actions()
-        self.knowledge_nase.delete_all_objects()
-        self.knowledge_nase.delete_all_propositions()
-        self.knowledge_nase.delete_all_predicates()
-        self.knowledge_nase.delete_all_types()
+        self.knowledge_base.delete_all_actions()
+        self.knowledge_base.delete_all_objects()
+        self.knowledge_base.delete_all_propositions()
+        self.knowledge_base.delete_all_predicates()
+        self.knowledge_base.delete_all_types()
 
     def test_save_type_true(self):
-        succ = self.knowledge_nase.save_type(self.robot_type)
+        succ = self.knowledge_base.save_type(self.robot_type)
         self.assertTrue(succ)
-        dto = self.knowledge_nase.get_type("robot")
+        dto = self.knowledge_base.get_type("robot")
         self.assertEqual("robot", str(dto))
-        self.assertEqual(1, len(self.knowledge_nase.get_all_types()))
+        self.assertEqual(1, len(self.knowledge_base.get_all_types()))
 
     def test_delete_type_true(self):
-        self.knowledge_nase.save_type(self.robot_type)
-        self.assertEqual(1, len(self.knowledge_nase.get_all_types()))
-        succ = self.knowledge_nase.delete_type(self.robot_type)
+        self.knowledge_base.save_type(self.robot_type)
+        self.assertEqual(1, len(self.knowledge_base.get_all_types()))
+        succ = self.knowledge_base.delete_type(self.robot_type)
         self.assertTrue(succ)
-        self.assertEqual(0, len(self.knowledge_nase.get_all_types()))
+        self.assertEqual(0, len(self.knowledge_base.get_all_types()))
 
     def test_delete_type_false(self):
-        succ = self.knowledge_nase.delete_type(self.robot_type)
+        succ = self.knowledge_base.delete_type(self.robot_type)
         self.assertFalse(succ)
 
     def test_delete_all_types(self):
-        self.knowledge_nase.save_type(self.robot_type)
-        self.assertEqual(1, len(self.knowledge_nase.get_all_types()))
-        succ = self.knowledge_nase.delete_all_types()
+        self.knowledge_base.save_type(self.robot_type)
+        self.assertEqual(1, len(self.knowledge_base.get_all_types()))
+        succ = self.knowledge_base.delete_all_types()
         self.assertTrue(succ)
-        self.assertEqual(0, len(self.knowledge_nase.get_all_types()))
+        self.assertEqual(0, len(self.knowledge_base.get_all_types()))
 
     def test_save_object_true(self):
-        succ = self.knowledge_nase.save_object(self.rb1)
+        succ = self.knowledge_base.save_object(self.rb1)
         self.assertTrue(succ)
-        dto = self.knowledge_nase.get_object("rb1")
+        dto = self.knowledge_base.get_object("rb1")
         self.assertEqual("rb1 - robot", str(dto))
-        self.assertEqual(1, len(self.knowledge_nase.get_all_objects()))
+        self.assertEqual(1, len(self.knowledge_base.get_all_objects()))
+
+    def test_save_object_true_type_alrready_exists(self):
+        succ = self.knowledge_base.save_type(self.rb1.get_pddl_type())
+        self.assertTrue(succ)
+        succ = self.knowledge_base.save_object(self.rb1)
+        self.assertTrue(succ)
+        dto = self.knowledge_base.get_object("rb1")
+        self.assertEqual("rb1 - robot", str(dto))
+        self.assertEqual(1, len(self.knowledge_base.get_all_objects()))
 
     def test_delete_object_true(self):
-        self.knowledge_nase.save_object(self.rb1)
-        self.assertEqual(1, len(self.knowledge_nase.get_all_objects()))
-        succ = self.knowledge_nase.delete_object(self.rb1)
+        self.knowledge_base.save_object(self.rb1)
+        self.assertEqual(1, len(self.knowledge_base.get_all_objects()))
+        succ = self.knowledge_base.delete_object(self.rb1)
         self.assertTrue(succ)
-        self.assertEqual(0, len(self.knowledge_nase.get_all_objects()))
+        self.assertEqual(0, len(self.knowledge_base.get_all_objects()))
 
     def test_delete_object_false(self):
-        succ = self.knowledge_nase.delete_object(self.rb1)
+        succ = self.knowledge_base.delete_object(self.rb1)
         self.assertFalse(succ)
 
     def test_delete_all_objects(self):
-        self.knowledge_nase.save_object(self.rb1)
-        self.assertEqual(1, len(self.knowledge_nase.get_all_objects()))
-        succ = self.knowledge_nase.delete_all_objects()
+        self.knowledge_base.save_object(self.rb1)
+        self.assertEqual(1, len(self.knowledge_base.get_all_objects()))
+        succ = self.knowledge_base.delete_all_objects()
         self.assertTrue(succ)
-        self.assertEqual(0, len(self.knowledge_nase.get_all_objects()))
+        self.assertEqual(0, len(self.knowledge_base.get_all_objects()))
 
     def test_save_predicate_true(self):
-        succ = self.knowledge_nase.save_predicate(self.robot_at)
+        succ = self.knowledge_base.save_predicate(self.robot_at)
         self.assertTrue(succ)
-        dto = self.knowledge_nase.get_predicate("robot_at")
+        dto = self.knowledge_base.get_predicate("robot_at")
         self.assertEqual("(robot_at ?r0 - robot ?w1 - wp)", str(dto))
-        self.assertEqual(1, len(self.knowledge_nase.get_all_predicates()))
+        self.assertEqual(1, len(self.knowledge_base.get_all_predicates()))
 
     def test_delete_predicate_true(self):
-        self.knowledge_nase.save_predicate(self.robot_at)
-        self.assertEqual(1, len(self.knowledge_nase.get_all_predicates()))
-        succ = self.knowledge_nase.delete_predicate(self.robot_at)
+        self.knowledge_base.save_predicate(self.robot_at)
+        self.assertEqual(1, len(self.knowledge_base.get_all_predicates()))
+        succ = self.knowledge_base.delete_predicate(self.robot_at)
         self.assertTrue(succ)
-        self.assertEqual(0, len(self.knowledge_nase.get_all_predicates()))
+        self.assertEqual(0, len(self.knowledge_base.get_all_predicates()))
 
     def test_delete_predicate_false(self):
-        succ = self.knowledge_nase.delete_predicate(self.robot_at)
+        succ = self.knowledge_base.delete_predicate(self.robot_at)
         self.assertFalse(succ)
 
     def test_delete_all_predicates(self):
-        self.knowledge_nase.save_predicate(self.robot_at)
-        self.assertEqual(1, len(self.knowledge_nase.get_all_predicates()))
-        succ = self.knowledge_nase.delete_all_predicates()
+        self.knowledge_base.save_predicate(self.robot_at)
+        self.assertEqual(1, len(self.knowledge_base.get_all_predicates()))
+        succ = self.knowledge_base.delete_all_predicates()
         self.assertTrue(succ)
-        self.assertEqual(0, len(self.knowledge_nase.get_all_predicates()))
+        self.assertEqual(0, len(self.knowledge_base.get_all_predicates()))
 
     def test_save_proposition_no_goal(self):
-        succ = self.knowledge_nase.save_proposition(self.rb1_robot_at)
+        succ = self.knowledge_base.save_proposition(self.rb1_robot_at)
         self.assertTrue(succ)
-        dto = self.knowledge_nase.get_propositions_no_goals()[0]
+        dto = self.knowledge_base.get_propositions_no_goals()[0]
         self.assertEqual("(robot_at rb1 wp1)", str(dto))
         self.assertEqual(
-            1, len(self.knowledge_nase.get_propositions_no_goals()))
+            1, len(self.knowledge_base.get_propositions_no_goals()))
 
     def test_save_proposition_goal(self):
-        succ = self.knowledge_nase.save_proposition(
+        succ = self.knowledge_base.save_proposition(
             self.rb1_wp2_wp_checked_goal)
         self.assertTrue(succ)
-        dto = self.knowledge_nase.get_propositions_goals()[0]
+        dto = self.knowledge_base.get_propositions_goals()[0]
         self.assertEqual("(wp_checked rb1 wp2)", str(dto))
         self.assertEqual(
-            1, len(self.knowledge_nase.get_propositions_goals()))
+            1, len(self.knowledge_base.get_propositions_goals()))
 
     def test_delete_proposition_true(self):
-        self.knowledge_nase.save_proposition(self.rb1_robot_at)
-        self.assertEqual(1, len(self.knowledge_nase.get_all_propositions()))
-        succ = self.knowledge_nase.delete_proposition(self.rb1_robot_at)
+        self.knowledge_base.save_proposition(self.rb1_robot_at)
+        self.assertEqual(1, len(self.knowledge_base.get_all_propositions()))
+        succ = self.knowledge_base.delete_proposition(self.rb1_robot_at)
         self.assertTrue(succ)
-        self.assertEqual(0, len(self.knowledge_nase.get_all_propositions()))
+        self.assertEqual(0, len(self.knowledge_base.get_all_propositions()))
 
     def test_delete_proposition_false(self):
-        succ = self.knowledge_nase.delete_proposition(self.rb1_robot_at)
+        succ = self.knowledge_base.delete_proposition(self.rb1_robot_at)
         self.assertFalse(succ)
 
     def test_delete_all_propositions(self):
-        self.knowledge_nase.save_proposition(self.rb1_robot_at)
-        self.assertEqual(1, len(self.knowledge_nase.get_all_propositions()))
-        succ = self.knowledge_nase.delete_all_propositions()
+        self.knowledge_base.save_proposition(self.rb1_robot_at)
+        self.assertEqual(1, len(self.knowledge_base.get_all_propositions()))
+        succ = self.knowledge_base.delete_all_propositions()
         self.assertTrue(succ)
-        self.assertEqual(0, len(self.knowledge_nase.get_all_propositions()))
+        self.assertEqual(0, len(self.knowledge_base.get_all_propositions()))
 
     def test_save_action_true(self):
-        succ = self.knowledge_nase.save_action(self.navigation_action)
+        succ = self.knowledge_base.save_action(self.navigation_action)
         self.assertTrue(succ)
-        dto = self.knowledge_nase.get_action("navigation")
+        dto = self.knowledge_base.get_action("navigation")
         self.assertEqual("""\
 (:durative-action navigation
 \t:parameters ( ?r - robot ?s - wp ?d - wp)
@@ -194,22 +203,22 @@ class TestDtoMsgParser(unittest.TestCase):
 \t)
 )""",
                          str(dto))
-        self.assertEqual(1, len(self.knowledge_nase.get_all_actions()))
+        self.assertEqual(1, len(self.knowledge_base.get_all_actions()))
 
     def test_delete_action_true(self):
-        self.knowledge_nase.save_action(self.navigation_action)
-        self.assertEqual(1, len(self.knowledge_nase.get_all_actions()))
-        succ = self.knowledge_nase.delete_action(self.navigation_action)
+        self.knowledge_base.save_action(self.navigation_action)
+        self.assertEqual(1, len(self.knowledge_base.get_all_actions()))
+        succ = self.knowledge_base.delete_action(self.navigation_action)
         self.assertTrue(succ)
-        self.assertEqual(0, len(self.knowledge_nase.get_all_actions()))
+        self.assertEqual(0, len(self.knowledge_base.get_all_actions()))
 
     def test_delete_action_false(self):
-        succ = self.knowledge_nase.delete_action(self.navigation_action)
+        succ = self.knowledge_base.delete_action(self.navigation_action)
         self.assertFalse(succ)
 
     def test_delete_all_actions(self):
-        self.knowledge_nase.save_action(self.navigation_action)
-        self.assertEqual(1, len(self.knowledge_nase.get_all_actions()))
-        succ = self.knowledge_nase.delete_all_actions()
+        self.knowledge_base.save_action(self.navigation_action)
+        self.assertEqual(1, len(self.knowledge_base.get_all_actions()))
+        succ = self.knowledge_base.delete_all_actions()
         self.assertTrue(succ)
-        self.assertEqual(0, len(self.knowledge_nase.get_all_actions()))
+        self.assertEqual(0, len(self.knowledge_base.get_all_actions()))
