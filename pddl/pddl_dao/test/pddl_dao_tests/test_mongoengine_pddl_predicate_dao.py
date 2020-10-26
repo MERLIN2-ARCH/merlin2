b4cf22e434282fca7e40663a67666c85cb1daca8
Miguel Ángel González Santamarta <mgonzs13@estudiantes.unileon.es>
2020-10-26 17:56:59 +0100

    fix

--- pddl/pddl_dao/test/pddl_dao_tests/test_mongoengine_pddl_predicate_dao.py
+++ pddl/pddl_dao/test/pddl_dao_tests/test_mongoengine_pddl_predicate_dao.py
@@ -13,8 +13,8 @@ class TestMongoenginePddlPredicateDao(TestPddlPredicateDao):
 
         pddl_dao_factory.set_uri("mongodb://localhost:27017/merlin2_tests")
 
-        self.pddl_object_dao = pddl_dao_factory.create_pddl_object_dao()
         self.pddl_type_dao = pddl_dao_factory.create_pddl_type_dao()
+        self.pddl_predicate_dao = pddl_dao_factory.create_pddl_predicate_dao()
 
 
 del(TestPddlPredicateDao)
