7f050b74b262068a2a13b52b7e5c5364a30c664d
Miguel Ángel González Santamarta <mgonzs13@estudiantes.unileon.es>
2020-10-26 17:57:07 +0100

    merlin2 tests created

---
+++ pddl/pddl_dao/test/pddl_dao_tests/test_merlin2_pddl_object_dao.py
@@ -0,0 +1,28 @@
+
+from test_pddl_dao_basic.test_pddl_object_dao import TestPddlObjectDao
+from pddl_dao.pddl_dao_factory.pddl_dao_factory_facory import PddlDaoFactoryFactory
+from ros1_node.node import Node
+import rclpy
+
+
+class TestMerlin2PddlObjectDao(TestPddlObjectDao):
+
+    def setUp(self):
+        super().setUp()
+
+        rclpy.init()
+        self.node = Node("test_merlin2_pddl_object_dao_node")
+        pddl_dao_factory_facory = PddlDaoFactoryFactory()
+        pddl_dao_factory = pddl_dao_factory_facory.create_pddl_dao_factory(
+            pddl_dao_factory_facory.pddl_dao_families.MERLIN2, node=self.node)
+
+        self.pddl_type_dao = pddl_dao_factory.create_pddl_type_dao()
+        self.pddl_object_dao = pddl_dao_factory.create_pddl_object_dao()
+
+    def tearDown(self):
+        super().tearDown()
+        self.node.destroy_node()
+        rclpy.shutdown()
+
+
+del(TestPddlObjectDao)
---
+++ pddl/pddl_dao/test/pddl_dao_tests/test_merlin2_pddl_type_dao.py
@@ -0,0 +1,27 @@
+
+from test_pddl_dao_basic.test_pddl_type_dao import TestPddlTypeDao
+from pddl_dao.pddl_dao_factory.pddl_dao_factory_facory import PddlDaoFactoryFactory
+from ros1_node.node import Node
+import rclpy
+
+
+class TestMerlin2PddlTypeDao(TestPddlTypeDao):
+
+    def setUp(self):
+        super().setUp()
+
+        rclpy.init()
+        self.node = Node("test_merlin2_pddl_type_dao_node")
+        pddl_dao_factory_facory = PddlDaoFactoryFactory()
+        pddl_dao_factory = pddl_dao_factory_facory.create_pddl_dao_factory(
+            pddl_dao_factory_facory.pddl_dao_families.MERLIN2, node=self.node)
+
+        self.pddl_type_dao = pddl_dao_factory.create_pddl_type_dao()
+
+    def tearDown(self):
+        super().tearDown()
+        self.node.destroy_node()
+        rclpy.shutdown()
+
+
+del(TestPddlTypeDao)
