ffa48440b8fa3f44a43b263da6cbe219bcad1633
Miguel Ángel González Santamarta <mgons@unileon.es>
2020-12-06 20:22:55 +0100

    uri args removed

--- pddl/pddl_dao/pddl_dao/pddl_dao_factory/pddl_dao_factories/mongoengine_pddl_dao_factory.py
+++ pddl/pddl_dao/pddl_dao/pddl_dao_factory/pddl_dao_factories/mongoengine_pddl_dao_factory.py
@@ -15,7 +15,7 @@ from pddl_dao.pddl_dao_factory.pddl_dao_factories.pddl_dao_factory import PddlDa
 class MongoenginePddlDaoFactory(PddlDaoFactory):
     """ Mongoengine Pddl Dao Facory Class """
 
-    def __init__(self, uri: str = None):
+    def __init__(self, uri: str = "mongodb://localhost:27017/merlin2"):
         self.set_uri(uri)
 
     def get_uri(self) -> str:
@@ -36,55 +36,34 @@ class MongoenginePddlDaoFactory(PddlDaoFactory):
 
         self._uri = uri
 
-    def create_pddl_type_dao(self, uri: str = None) -> MongoenginePddlTypeDao:
+    def create_pddl_type_dao(self) -> MongoenginePddlTypeDao:
         """ create a mongoengine pddl dao type object
-
-        Args:
-            uri (str, optional): Mongo uri. Defaults to None.
 
         Returns:
             MongoenginePddlTypeDao: mongoengine dao for pddl type
         """
 
-        aux_uri = self._uri
-        if not uri is None:
-            aux_uri = uri
+        return MongoenginePddlTypeDao(uri=self._uri)
 
-        return MongoenginePddlTypeDao(uri=aux_uri)
-
-    def create_pddl_predicate_dao(self, uri: str = None) -> MongoenginePddlPredicateDao:
+    def create_pddl_predicate_dao(self) -> MongoenginePddlPredicateDao:
         """ create a mongoengine pddl dao predicate object
-
-        Args:
-            uri (str, optional): Mongo uri. Defaults to None.
 
         Returns:
             MongoenginePddlPredicateDao: mongoengine dao for pddl predicate
         """
 
-        aux_uri = self._uri
-        if not uri is None:
-            aux_uri = uri
+        return MongoenginePddlPredicateDao(uri=self._uri)
 
-        return MongoenginePddlPredicateDao(uri=aux_uri)
-
-    def create_pddl_action_dao(self, uri: str = None) -> MongoenginePddlActionDao:
+    def create_pddl_action_dao(self) -> MongoenginePddlActionDao:
         """ create a mongoengine pddl dao action object
-
-        Args:
-            uri (str, optional): Mongo uri. Defaults to None.
 
         Returns:
             MongoenginePddlActionDao: mongoengine dao for pddl action
         """
 
-        aux_uri = self._uri
-        if not uri is None:
-            aux_uri = uri
+        return MongoenginePddlActionDao(uri=self._uri)
 
-        return MongoenginePddlActionDao(uri=aux_uri)
-
-    def create_pddl_object_dao(self, uri: str = None) -> MongoenginePddlObjectDao:
+    def create_pddl_object_dao(self) -> MongoenginePddlObjectDao:
         """ create a mongoengine pddl dao object object
 
         Args:
@@ -94,24 +73,13 @@ class MongoenginePddlDaoFactory(PddlDaoFactory):
             MongoenginePddlObjectDao: mongoengine dao for pddl object
         """
 
-        aux_uri = self._uri
-        if not uri is None:
-            aux_uri = uri
+        return MongoenginePddlObjectDao(uri=self._uri)
 
-        return MongoenginePddlObjectDao(uri=aux_uri)
-
-    def create_pddl_proposition_dao(self, uri: str = None) -> MongoenginePddlPropositionDao:
+    def create_pddl_proposition_dao(self) -> MongoenginePddlPropositionDao:
         """ create a mongoengine pddl dao proposition object
-
-        Args:
-            uri (str, optional): Mongo uri. Defaults to None.
 
         Returns:
             MongoenginePddlPropositionDao: mongoengine dao for pddl proposition
         """
 
-        aux_uri = self._uri
-        if not uri is None:
-            aux_uri = uri
-
-        return MongoenginePddlPropositionDao(uri=aux_uri)
+        return MongoenginePddlPropositionDao(uri=self._uri)
