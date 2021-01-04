3030a796dd7effa4eeeae1f65c50994b3d5feb27
Miguel Ángel González Santamarta <mgons@unileon.es>
2021-01-04 21:29:52 +0100

    cancel_callback fixed

--- custom_ros2/custom_ros2/action_queue_server.py
+++ custom_ros2/custom_ros2/action_queue_server.py
@@ -10,9 +10,7 @@ class ActionQueueServer(ActionServer):
     def __init__(self, node, s_type, s_name, execute_callback, cancel_callback=None):
 
         self.__user_execute_callback = execute_callback
-
-        if cancel_callback is None:
-            cancel_callback = self.__cancel_callback
+        self.__user_cancel_callback = cancel_callback
 
         self._goal_queue = collections.deque()
         self._goal_queue_lock = threading.Lock()
@@ -36,6 +34,10 @@ class ActionQueueServer(ActionServer):
         return GoalResponse.ACCEPT
 
     def __cancel_callback(self, goal_handle):
+
+        if self.__user_cancel_callback is not None:
+            self.__user_cancel_callback()
+
         return CancelResponse.ACCEPT
 
     def __execute_callback(self, goal_handle):
--- custom_ros2/custom_ros2/action_single_server.py
+++ custom_ros2/custom_ros2/action_single_server.py
@@ -7,8 +7,7 @@ class ActionSingleServer(ActionServer):
 
     def __init__(self, node, s_type, s_name, execute_callback, cancel_callback=None):
 
-        if cancel_callback is None:
-            cancel_callback = self.__cancel_callback
+        self.__user_cancel_callback = cancel_callback
 
         self._goal_handle = None
         self._goal_lock = threading.Lock()
@@ -17,7 +16,7 @@ class ActionSingleServer(ActionServer):
                          execute_callback=execute_callback,
                          goal_callback=self.__goal_callback,
                          handle_accepted_callback=self.__handle_accepted_callback,
-                         cancel_callback=cancel_callback)
+                         cancel_callback=self.__cancel_callback)
 
     def __goal_callback(self, goal_request):
         return GoalResponse.ACCEPT
@@ -31,4 +30,8 @@ class ActionSingleServer(ActionServer):
         goal_handle.execute()
 
     def __cancel_callback(self, goal):
+
+        if self.__user_cancel_callback is not None:
+            self.__user_cancel_callback()
+
         return CancelResponse.ACCEPT
