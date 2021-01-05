961eaebc44ff17c87aaca0007201feaba3f6e773
Miguel Ángel González Santamarta <mgons@unileon.es>
2021-01-05 17:47:49 +0100

    asyncio replcaed by the new ActionClient

--- merlin2_plan_sys/merlin2_executor/merlin2_executor/merlin2_executor_node.py
+++ merlin2_plan_sys/merlin2_executor/merlin2_executor/merlin2_executor_node.py
@@ -1,23 +1,18 @@
 
 """ Merlin2 Executor Node """
 
-import asyncio
-from typing import List
 
 import rclpy
-from rclpy.action import ActionClient
-
-from action_msgs.msg import GoalStatus
 
 from merlin2_plan_sys_interfaces.srv import (
     GeneratePddl, GeneratePlan
 )
-from merlin2_plan_sys_interfaces.msg import PlanAction
 from merlin2_plan_sys_interfaces.action import DispatchPlan, Execute
 
 from custom_ros2 import (
     Node,
-    ActionSingleServer
+    ActionSingleServer,
+    ActionClient
 )
 
 
@@ -31,7 +26,7 @@ class Merlin2ExecutorNode(Node):
         # service clients
         self.__pddl_generator_client = self.create_client(
             GeneratePddl, "generate_pddl")
-        self.__plan_client = self.create_client(
+        self.__planner_client = self.create_client(
             GeneratePlan, "generate_plan")
 
         # action client
@@ -42,121 +37,59 @@ class Merlin2ExecutorNode(Node):
         self.__action_server = ActionSingleServer(self,
                                                   Execute,
                                                   "execute",
-                                                  execute_callback=self.__execute_server
+                                                  execute_callback=self.__execute_server,
+                                                  cancel_callback=self.__cancel_callback
                                                   )
 
     def destroy(self):
-        """ destroy node method
-        """
+        """ destroy node method """
 
         self.__action_server.destroy()
         super().destroy_node()
+
+    def __cancel_callback(self):
+        if self.__plan_dispatcher_client.is_working():
+            self.__plan_dispatcher_client.cancel_goal()
 
     def __execute_server(self, goal_handle):
         """ execute action server
 
         Args:
-            goal_handle ([type]): goal_handle
+            goal_handle: goal_handle
         """
 
         result = Execute.Result()
 
-        pddl_generated = asyncio.run(self.generate_pddl())
+        # PDDL Generator
+        req = GeneratePddl.Request()
+        self.__pddl_generator_client.wait_for_service()
+        pddl_generated = self.__pddl_generator_client.call(req)
         self.get_logger().info(pddl_generated.domain)
         self.get_logger().info(pddl_generated.problem)
         result.generate_pddl = True
 
-        plan = asyncio.run(
-            self.plan(pddl_generated.domain, pddl_generated.problem))
+        # Planner
+        req = GeneratePlan.Request()
+        req.domain = pddl_generated.domain
+        req.problem = pddl_generated.problem
+        self.__planner_client.wait_for_service()
+        plan = self.__planner_client.call(req)
         self.get_logger().info(str(plan.has_solution))
         self.get_logger().info(str(plan.plan))
         result.generate_plan = True
 
-        dispatch_plan_succ = asyncio.run(self.dispatch_plan(plan.plan))
-        result.dispatch_plan = dispatch_plan_succ
+        # Plan Dispatcher
+        goal = DispatchPlan.Goal()
+        goal.plan = plan.plan
+        self.__plan_dispatcher_client.wait_for_server()
+        self.__plan_dispatcher_client.send_goal(goal)
+        self.__plan_dispatcher_client.wait_for_result()
+        self.get_logger().info(str(self.__plan_dispatcher_client.get_status()))
+        result.dispatch_plan = self.__plan_dispatcher_client.is_succeeded()
 
         goal_handle.succeed()
 
         return result
-
-    async def generate_pddl(self) -> GeneratePddl.Response:
-        """ asyn generate pddl method
-
-        Returns:
-            GeneratePddl.Response: pddl (domain and problem)
-        """
-
-        req = GeneratePddl.Request()
-        self.__pddl_generator_client.wait_for_service()
-        future = self.__pddl_generator_client.call_async(req)
-
-        try:
-            await future
-        except Exception as e:
-            self.get_logger().info("Service call failed %r" % (e,))
-
-        return future.result()
-
-    async def plan(self, domain: str, problem: str) -> GeneratePlan.Response:
-        """ asyn plan method
-
-        Args:
-            domain (str): str of pddl domain
-            problem (str): str of pddl problem
-
-        Returns:
-            GeneratePlan.Response: pddl (plan)
-        """
-
-        req = GeneratePlan.Request()
-        req.domain = domain
-        req.problem = problem
-        self.__plan_client.wait_for_service()
-        future = self.__plan_client.call_async(req)
-
-        try:
-            await future
-        except Exception as e:
-            self.get_logger().info("Service call failed %r" % (e,))
-
-        return future.result()
-
-    async def dispatch_plan(self, plan: List[PlanAction]) -> bool:
-        """ dispatch a plan
-
-        Args:
-            plan (List[PlanAction]): list of actions that compose the plan
-
-        Returns:
-            bool: succeed
-        """
-
-        goal = DispatchPlan.Goal()
-        goal.plan = plan
-        self.__plan_dispatcher_client.wait_for_server()
-        send_goal_future = self.__plan_dispatcher_client.send_goal_async(goal)
-
-        try:
-            await send_goal_future
-            goal_handle = send_goal_future.result()
-
-            if not goal_handle.accepted:
-                return False
-
-            get_result_future = goal_handle.get_result_async()
-            await get_result_future
-
-            result = get_result_future.result().result
-            status = get_result_future.result().status
-
-            if status == GoalStatus.STATUS_SUCCEEDED:
-                return True
-            else:
-                return False
-
-        except Exception as e:
-            self.get_logger().info("Action call failed %r" % (e,))
-            return False
 
 
 def main(args=None):
