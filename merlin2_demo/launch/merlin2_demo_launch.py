9faffb18510694f14456aea54b237cc5e77cbb9c
Miguel Ángel González Santamarta <mgons@unileon.es>
2021-01-21 17:27:21 +0100

    new includes + new action

--- merlin2_demo/launch/merlin2_demo_launch.py
+++ merlin2_demo/launch/merlin2_demo_launch.py
@@ -13,6 +13,11 @@ def generate_launch_description():
         'merlin2_planning_layer')
     topo_nav_share_dir = get_package_share_directory(
         'ros2_topological_nav')
+    speech_recognition_share_dir = get_package_share_directory(
+        'ros2_speech_recognition')
+    tts_share_dir = get_package_share_directory(
+        'ros2_text_to_speech')
+
     stdout_linebuf_envvar = SetEnvironmentVariable(
         'RCUTILS_CONSOLE_STDOUT_LINE_BUFFERED', '1')
 
@@ -24,6 +29,12 @@ def generate_launch_description():
         package='merlin2_basic_actions',
         executable='merlin2_navigation_action',
         name='merlin2_navigation_action'
+    )
+
+    merlin2_hi_navigation_action_cmd = Node(
+        package='merlin2_demo',
+        executable='merlin2_hi_navigation_action',
+        name='merlin2_hi_navigation_action'
     )
 
     merlin2_demo_node_cmd = Node(
@@ -42,6 +53,16 @@ def generate_launch_description():
             os.path.join(topo_nav_share_dir, 'ros2_topo_nav_launch.py'))
     )
 
+    speech_recognition_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(speech_recognition_share_dir, 'ros2_speech_recognition_launch.py'))
+    )
+
+    tts_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(tts_share_dir, 'ros2_text_to_speech_launch.py'))
+    )
+
     merlin2_planning_layer_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(planning_layer_share_dir, 'merlin2_planning_layer_launch.py'))
@@ -50,8 +71,14 @@ def generate_launch_description():
     ld = LaunchDescription()
 
     ld.add_action(stdout_linebuf_envvar)
+
     ld.add_action(topo_nav_cmd)
+    ld.add_action(speech_recognition_cmd)
+    ld.add_action(tts_cmd)
+
     ld.add_action(merlin2_navigation_action_cmd)
+    ld.add_action(merlin2_hi_navigation_action_cmd)
+
     ld.add_action(merlin2_planning_layer_cmd)
     ld.add_action(merlin2_demo_node_cmd)
 
