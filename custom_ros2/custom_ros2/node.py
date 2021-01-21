feec2634a0c3c5b056ab3c3389be852c6d1373a8
Miguel Ángel González Santamarta <mgons@unileon.es>
2021-01-21 13:46:29 +0100

    fixes (executor threads starts and wait for init node)

--- custom_ros2/custom_ros2/node.py
+++ custom_ros2/custom_ros2/node.py
@@ -1,6 +1,7 @@
 
 """ ROS2 Node to simulate ROS1 Node """
 
+import time
 from threading import Thread
 from rclpy.node import Node as Node2
 from rclpy.executors import MultiThreadedExecutor, Executor
@@ -10,16 +11,24 @@ class Node(Node2):
     """ Node Class """
 
     def __init__(self, node_name, executor: Executor = None):
-        super().__init__(node_name)
+        self.__node_init = False
 
         if not executor:
             self._executor = MultiThreadedExecutor()
+        else:
+            self._executor = executor
 
         self._spin_thread = Thread(target=self._run_executor, daemon=True)
         self._spin_thread.start()
 
+        super().__init__(node_name)
+        self.__node_init = True
+
     def _run_executor(self):
         """ run an executer with self (node) """
+
+        while not self.__node_init:
+            time.sleep(0.1)
 
         self._executor.add_node(self)
         try:
