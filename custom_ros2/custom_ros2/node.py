0599ecf0a7f7ebd869b864ddae055fa6001b5752
Miguel Ángel González Santamarta <mgons@unileon.es>
2021-01-21 17:44:41 +0100

    fix, sleep removed

--- custom_ros2/custom_ros2/node.py
+++ custom_ros2/custom_ros2/node.py
@@ -1,7 +1,6 @@
 
 """ ROS2 Node to simulate ROS1 Node """
 
-import time
 from threading import Thread
 from rclpy.node import Node as Node2
 from rclpy.executors import MultiThreadedExecutor, Executor
@@ -11,7 +10,8 @@ class Node(Node2):
     """ Node Class """
 
     def __init__(self, node_name, executor: Executor = None):
-        self.__node_init = False
+
+        super().__init__(node_name)
 
         if not executor:
             self._executor = MultiThreadedExecutor()
@@ -21,14 +21,8 @@ class Node(Node2):
         self._spin_thread = Thread(target=self._run_executor, daemon=True)
         self._spin_thread.start()
 
-        super().__init__(node_name)
-        self.__node_init = True
-
     def _run_executor(self):
         """ run an executer with self (node) """
-
-        while not self.__node_init:
-            time.sleep(0.1)
 
         self._executor.add_node(self)
         try:
