2cd34451cf9ed799e2c547a5ed2bfccb20727758
Miguel Ángel González Santamarta <mgons@unileon.es>
2021-01-23 15:40:49 +0100

    new action server  versions

--- custom_ros2/custom_ros2/action_queue_server.py
+++ custom_ros2/custom_ros2/action_queue_server.py
@@ -3,69 +3,50 @@
 
 import collections
 import threading
-
-from rclpy.action import ActionServer, CancelResponse, GoalResponse
-from rclpy.callback_groups import ReentrantCallbackGroup
+from .action_server import ActionServer
 
 
 class ActionQueueServer(ActionServer):
     """ Action Queue Goal Server Class """
 
-    def __init__(self, node, s_type, s_name, execute_callback, cancel_callback=None):
+    def __init__(self, node, action_type, action_name, execute_callback, cancel_callback=None):
 
-        self.__user_execute_callback = execute_callback
-        self.__user_cancel_callback = cancel_callback
+        self.__queue_user_execute_callback = execute_callback
 
-        self._goal_queue = collections.deque()
-        self._goal_queue_lock = threading.Lock()
-        self._current_goal = None
+        self.__goal_queue = collections.deque()
+        self.__goal_queue_lock = threading.Lock()
 
-        super().__init__(node, s_type, s_name,
-                         execute_callback=self.__execute_callback,
-                         goal_callback=self.__goal_callback,
-                         handle_accepted_callback=self.__handle_accepted_callback,
-                         cancel_callback=cancel_callback,
-                         callback_group=ReentrantCallbackGroup())
+        super().__init__(node, action_type, action_name,
+                         execute_callback=self.__queue_execute_callback,
+                         handle_accepted_callback=self.__queue_handle_accepted_callback,
+                         cancel_callback=cancel_callback)
 
-    def __handle_accepted_callback(self, goal_handle):
+    def __queue_handle_accepted_callback(self, goal_handle):
         """
             handle accepted calback for a queue goal server
             goals are added to a queue
         """
 
-        with self._goal_queue_lock:
-            if self._current_goal is not None:
-                self._goal_queue.append(goal_handle)
+        with self.__goal_queue_lock:
+            if self._goal_handle is not None:
+                self.__goal_queue.append(goal_handle)
             else:
-                self._current_goal = goal_handle
-                self._current_goal.execute()
+                self._goal_handle = goal_handle
+                self._goal_handle.execute()
 
-    def __goal_callback(self, goal_request):
-        """ goal callback for a queue goal server """
-
-        return GoalResponse.ACCEPT
-
-    def __cancel_callback(self, goal_handle):
-        """ cancel calback for a queue goal server """
-
-        if self.__user_cancel_callback is not None:
-            self.__user_cancel_callback()
-
-        return CancelResponse.ACCEPT
-
-    def __execute_callback(self, goal_handle):
-        """ 
+    def __queue_execute_callback(self, goal_handle):
+        """
             execute callback for a queue goal server
             when a goals ends, next goal is got from the queue
         """
 
         try:
-            return self.__user_execute_callback(goal_handle)
+            return self.__queue_user_execute_callback(goal_handle)
 
         finally:
-            with self._goal_queue_lock:
+            with self.__goal_queue_lock:
                 try:
-                    self._current_goal = self._goal_queue.popleft()
-                    self._current_goal.execute()
+                    self._goal_handle = self.__goal_queue.popleft()
+                    self._goal_handle.execute()
                 except IndexError:
-                    self._current_goal = None
+                    self._goal_handle = None
--- custom_ros2/custom_ros2/action_single_server.py
+++ custom_ros2/custom_ros2/action_single_server.py
@@ -2,52 +2,30 @@
 """ Custom action server that treats only one goals at the same time """
 
 import threading
-from rclpy.action import ActionServer, CancelResponse, GoalResponse
-from rclpy.callback_groups import ReentrantCallbackGroup
+from .action_server import ActionServer
 
 
 class ActionSingleServer(ActionServer):
     """ Action Single Goal Server Class """
 
-    def __init__(self, node, s_type, s_name, execute_callback, cancel_callback=None):
+    def __init__(self, node, action_type, action_name, execute_callback, cancel_callback=None):
 
-        self.__user_cancel_callback = cancel_callback
+        self.__goal_lock = threading.Lock()
 
-        self._goal_handle = None
-        self._goal_lock = threading.Lock()
-
-        super().__init__(node, s_type, s_name,
+        super().__init__(node, action_type, action_name,
                          execute_callback=execute_callback,
-                         goal_callback=self.__goal_callback,
-                         handle_accepted_callback=self.__handle_accepted_callback,
-                         cancel_callback=self.__cancel_callback,
-                         callback_group=ReentrantCallbackGroup())
+                         handle_accepted_callback=self.__single_handle_accepted_callback,
+                         cancel_callback=cancel_callback)
 
-    def __goal_callback(self, goal_request):
-        """ goal callback for a single goal server """
-
-        return GoalResponse.ACCEPT
-
-    def __handle_accepted_callback(self, goal_handle):
+    def __single_handle_accepted_callback(self, goal_handle):
         """
             handle accepted calback for a single goal server
             only one goal can be treated
             if other goal is send, old goal is aborted and replaced with the new one
         """
 
-        with self._goal_lock:
+        with self.__goal_lock:
             if self._goal_handle is not None and self._goal_handle.is_active:
                 self._goal_handle.abort()
+
             self._goal_handle = goal_handle
-
-        goal_handle.execute()
-
-    def __cancel_callback(self, goal):
-        """ cancel calback for a single goal server """
-
-        if self.__user_cancel_callback is not None:
-            self.__user_cancel_callback()
-
-        self._goal_handle = None
-
-        return CancelResponse.ACCEPT
