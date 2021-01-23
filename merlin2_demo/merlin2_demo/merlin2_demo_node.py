7c08ffdb525ebd5c286b7fc4349051067480fa4a
Miguel Ángel González Santamarta <mgons@unileon.es>
2021-01-23 16:49:18 +0100

    Merlin2MissionNode aaplied to demo nodes

--- merlin2_demo/merlin2_demo/merlin2_demo_node.py
+++ merlin2_demo/merlin2_demo/merlin2_demo_node.py
@@ -1,7 +1,7 @@
 
 import rclpy
 
-from merlin2_goal_dispatcher import Merlin2GoalDispatcher
+from merlin2_mission import Merlin2MissionNode
 
 from merlin2_basic_actions.merlin2_basic_types import (
     wp_type,
@@ -19,57 +19,41 @@ from pddl_dto import (
 
 from .pddl import person_attended
 
-from custom_ros2 import Node
 
-
-class Merlin2DemoNode(Node):
+class Merlin2DemoNode(Merlin2MissionNode):
 
     def __init__(self):
-
         super().__init__("demo_node")
 
-        self.goal_dispatcher = Merlin2GoalDispatcher(self)
-        self.pddl_dao_factory = self.goal_dispatcher.get_pddl_factory()
-
-        pddl_object_dao = self.pddl_dao_factory.create_pddl_object_dao()
-        pddl_proposition_dao = self.pddl_dao_factory.create_pddl_proposition_dao()
-
+    def create_objects(self):
         kitchen = PddlObjectDto(wp_type, "kitchen")
         bedroom = PddlObjectDto(wp_type, "bedroom")
-        livingroom = PddlObjectDto(wp_type, "livingroom")
-        entrance = PddlObjectDto(wp_type, "entrance")
+        self.livingroom = PddlObjectDto(wp_type, "livingroom")
+        self.entrance = PddlObjectDto(wp_type, "entrance")
         bathroom = PddlObjectDto(wp_type, "bathroom")
-        miguel = PddlObjectDto(person_type, "miguel")
-        objects = [kitchen, bedroom, livingroom, entrance, bathroom, miguel]
+        self.miguel = PddlObjectDto(person_type, "miguel")
+        objects = [kitchen, bedroom, self.livingroom,
+                   self.entrance, bathroom, self.miguel]
+        return objects
 
-        robot_at_prop = PddlPropositionDto(robot_at, [entrance])
-        miguel_at_prop = PddlPropositionDto(person_at, [miguel, livingroom])
-        self.person_attended_goal = PddlPropositionDto(
-            person_attended, [miguel], is_goal=True)
-        propositions = [robot_at_prop, miguel_at_prop]
-
-        pddl_object_dao.delete_all()
-        pddl_proposition_dao.delete_all()
-
-        for pddl_object_dto in objects:
-            pddl_object_dao.save(pddl_object_dto)
-
-        for pddl_proposition_dto in propositions:
-            pddl_proposition_dao.save(pddl_proposition_dto)
+    def create_propositions(self):
+        miguel_at_prop = PddlPropositionDto(
+            person_at, [self.miguel, self.livingroom])
+        robot_at_prop = PddlPropositionDto(robot_at, [self.entrance])
+        return [robot_at_prop, miguel_at_prop]
 
     def execute(self):
-
-        succeed = self.goal_dispatcher.execute_goals(
-            [self.person_attended_goal])
+        self.get_logger().info("EXECUTING MISSION")
+        person_attended_goal = PddlPropositionDto(
+            person_attended, [self.miguel], is_goal=True)
+        succeed = self.execute_goal(person_attended_goal)
         self.get_logger().info(str(succeed))
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    node = Merlin2DemoNode()
-
-    node.execute()
+    Merlin2DemoNode()
 
     rclpy.shutdown()
 
--- merlin2_demo/merlin2_demo/merlin2_mdpi_node.py
+++ merlin2_demo/merlin2_demo/merlin2_mdpi_node.py
@@ -5,7 +5,7 @@ from random import seed, randint
 import threading
 import rclpy
 
-from merlin2_goal_dispatcher import Merlin2GoalDispatcher
+from merlin2_mission import Merlin2MissionNode
 
 from merlin2_basic_actions.merlin2_basic_types import (
     wp_type,
@@ -19,19 +19,15 @@ from pddl_dto import (
     PddlPropositionDto
 )
 
-from custom_ros2 import Node
 
 from .pddl import wp_checked
 
 
-class Merlin2MdpiNode(Node):
+class Merlin2MdpiNode(Merlin2MissionNode):
 
     def __init__(self):
 
-        super().__init__("mdpi_node")
-
-        self.goal_dispatcher = Merlin2GoalDispatcher(self)
-        self.pddl_dao_factory = self.goal_dispatcher.get_pddl_factory()
+        super().__init__("mdpi_node", run_mission=False)
 
         # parameters
         total_points_param_name = "total_points"
@@ -48,28 +44,18 @@ class Merlin2MdpiNode(Node):
             time_to_cancel_param_name).get_parameter_value().integer_value
         self.wp_list = []
 
-        # initial pddl
-        pddl_object_dao = self.pddl_dao_factory.create_pddl_object_dao()
-        pddl_proposition_dao = self.pddl_dao_factory.create_pddl_proposition_dao()
-
-        wp0 = PddlObjectDto(wp_type, "wp0")
+    def create_objects(self):
+        self.wp0 = PddlObjectDto(wp_type, "wp0")
         wp1 = PddlObjectDto(wp_type, "wp1")
         wp2 = PddlObjectDto(wp_type, "wp2")
         wp3 = PddlObjectDto(wp_type, "wp3")
-        self.anywhere = PddlObjectDto(wp_type, "anywhere")
-        objects = [wp0, wp1, wp2, wp3, self.anywhere]
+        objects = [self.wp0, wp1, wp2, wp3]
+        return objects
 
-        robot_at_prop = PddlPropositionDto(robot_at, [wp0])
+    def create_propositions(self):
+        robot_at_prop = PddlPropositionDto(robot_at, [self.wp0])
         propositions = [robot_at_prop]
-
-        pddl_object_dao.delete_all()
-        pddl_proposition_dao.delete_all()
-
-        for pddl_object_dto in objects:
-            pddl_object_dao.save(pddl_object_dto)
-
-        for pddl_proposition_dto in propositions:
-            pddl_proposition_dao.save(pddl_proposition_dto)
+        return propositions
 
     def init_points(self):
         self.wp_list = []
@@ -116,37 +102,28 @@ class Merlin2MdpiNode(Node):
             goal = PddlPropositionDto(wp_checked, [wp], is_goal=True)
 
             # send goal
-            thread = threading.Thread(target=self._check_wp, args=(goal,))
+            thread = threading.Thread(target=self.execute_goal, args=(goal,))
             thread.start()
 
             # cancel?
             if cancel:
+                self.get_logger().info(
+                    "CANCELING ------------------------------------")
                 i = 0
                 while i < self.time_to_cancel and thread.is_alive():
                     time.sleep(1)
                     i += 1
-                self.goal_dispatcher.cancel_goals()
+                self.cancel_goals()
 
             # wait for thread
             thread.join()
             pddl_proposition_dao.delete(goal)
-
-            # check robot_at props
-            # robot_at_prop = pddl_proposition_dao.get_by_predicate(
-            #    robot_at.get_predicate_name())
-
-            # if not robot_at_prop:
-            #    robot_at_prop = PddlPropositionDto(robot_at, [self.anywhere])
-            #    pddl_proposition_dao.save(robot_at_prop)
 
         # results
         end_t = time.time()
         total_t = end_t - start_t
         self.get_logger().info("TIME: " + str(total_t) +
                                " ------------------------------------")
-
-    def _check_wp(self, goal):
-        self.goal_dispatcher.execute_goals([goal])
 
 
 def main(args=None):
