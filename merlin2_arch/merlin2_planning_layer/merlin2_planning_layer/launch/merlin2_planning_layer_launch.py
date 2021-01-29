3208c8315ce63e1d85a63f0bbd8b1632c6393f02
Miguel Ángel González Santamarta <mgons@unileon.es>
2021-01-29 17:22:16 +0100

    launch param and kn node added

--- merlin2_arch/merlin2_planning_layer/merlin2_planning_layer/launch/merlin2_planning_layer_launch.py
+++ merlin2_arch/merlin2_planning_layer/merlin2_planning_layer/launch/merlin2_planning_layer_launch.py
@@ -2,7 +2,8 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable
-import ament_index_python
+from pddl_dao.pddl_dao_factory import PddlDaoFamilies
+from launch.conditions import LaunchConfigurationEquals
 
 
 def generate_launch_description():
@@ -10,6 +11,22 @@ def generate_launch_description():
 
     stdout_linebuf_envvar = SetEnvironmentVariable(
         'RCUTILS_CONSOLE_STDOUT_LINE_BUFFERED', '1')
+
+    #
+    # ARGS
+    #
+
+    pddl_dao_family = LaunchConfiguration('pddl_dao_family')
+    pddl_dao_family_cmd = DeclareLaunchArgument(
+        'pddl_dao_family',
+        default_value=str(int(PddlDaoFamilies.MONGOENGINE)),
+        description='DAO family')
+
+    mongoengine_uri = LaunchConfiguration('mongoengine_uri')
+    mongoengine_uri_cmd = DeclareLaunchArgument(
+        'mongoengine_uri',
+        default_value='mongodb://localhost:27017/merlin2',
+        description='MongoDB URI')
 
     #
     # NODES
@@ -20,6 +37,8 @@ def generate_launch_description():
         executable='pddl_generator_node',
         name='pddl_generator_node',
         namespace=namespace,
+        parameters=[{'pddl_dao_family': pddl_dao_family,
+                     'mongoengine_uri': mongoengine_uri}]
     )
 
     planner_node_cmd = Node(
@@ -34,6 +53,8 @@ def generate_launch_description():
         executable='plan_dispatcher_node',
         name='plan_dispatcher_node',
         namespace=namespace,
+        parameters=[{'pddl_dao_family': pddl_dao_family,
+                     'mongoengine_uri': mongoengine_uri}]
     )
 
     executor_node_cmd = Node(
@@ -43,13 +64,26 @@ def generate_launch_description():
         namespace=namespace,
     )
 
+    knowledge_base_node_cmd = Node(
+        package="merlin2_knowledge_base",
+        executable='knowledge_base_node',
+        name='knowledge_base_node',
+        namespace=namespace,
+        condition=LaunchConfigurationEquals(
+            'pddl_dao_family', str(int(PddlDaoFamilies.MERLIN2)))
+    )
+
     ld = LaunchDescription()
 
     ld.add_action(stdout_linebuf_envvar)
+
+    ld.add_action(pddl_dao_family_cmd)
+    ld.add_action(mongoengine_uri_cmd)
 
     ld.add_action(pddl_generator_node_cmd)
     ld.add_action(planner_node_cmd)
     ld.add_action(plan_dispatcher_node_cmd)
     ld.add_action(executor_node_cmd)
+    ld.add_action(knowledge_base_node_cmd)
 
     return ld
