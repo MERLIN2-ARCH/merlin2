36e55acc40acdf16418b3333014981cbd50c2ab0
Miguel Ángel González Santamarta <mgons@unileon.es>
2021-01-30 12:56:54 +0100

    action_state destroy method created

--- merlin2_arch/merlin2_executive_layer/merlin2_fsm_action/merlin2_fsm_action/merlin2_fsm_action.py
+++ merlin2_arch/merlin2_executive_layer/merlin2_fsm_action/merlin2_fsm_action/merlin2_fsm_action.py
@@ -1,18 +1,21 @@
 
 """ MERLIN2 FSM Action """
 
+from typing import Dict
 from merlin2_arch_interfaces.msg import PlanAction
 from merlin2_action.merlin2_action import Merlin2Action
 from ros2_fsm_viewer import Ros2FsmViewerPub
-from ros2_fsm.basic_fsm import StateMachine
+from ros2_fsm.basic_fsm import StateMachine, State
 from ros2_fsm.basic_fsm.shared_data import SharedData
-from ros2_fsm.ros2_states import BasicOutomes
+from ros2_fsm.ros2_states import BasicOutomes, AcionState
 
 
 class Merlin2FsmAction(Merlin2Action, StateMachine):
     """ Merlin2 FSM Action Class """
 
     def __init__(self, action_name: str):
+
+        self.__action_state_list = []
 
         Merlin2Action.__init__(self, action_name)
         StateMachine.__init__(self, [BasicOutomes.SUCC,
@@ -38,3 +41,18 @@ class Merlin2FsmAction(Merlin2Action, StateMachine):
 
     def cancel_action(self):
         self.cancel_state()
+
+    def destroy(self):
+        for action_state in self.__action_state_list:
+            action_state.destroy()
+        super().destroy()
+
+    def add_state(self,
+                  name: str,
+                  state: State,
+                  transitions: Dict[str, str] = None):
+
+        if isinstance(state, AcionState):
+            self.__action_state_list.append(state)
+
+        super().add_state(name, state, transitions)
