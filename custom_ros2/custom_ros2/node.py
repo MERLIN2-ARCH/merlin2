01ef5eb733fce14f99e3572523b0f236c0949d67
Miguel Ángel González Santamarta <mgons@unileon.es>
2021-02-05 13:09:41 +0100

    actions creation functions + WAKE

--- custom_ros2/custom_ros2/node.py
+++ custom_ros2/custom_ros2/node.py
@@ -4,12 +4,17 @@
 from threading import Thread
 from rclpy.node import Node as Node2
 from rclpy.executors import MultiThreadedExecutor, Executor
+from .actions import (
+    ActionClient,
+    ActionQueueServer,
+    ActionSingleServer
+)
 
 
 class Node(Node2):
     """ Node Class """
 
-    def __init__(self, node_name, namespace: str = "", executor: Executor = None):
+    def __init__(self, node_name: str, namespace: str = "", executor: Executor = None):
 
         super().__init__(node_name, namespace=namespace)
 
@@ -34,7 +39,67 @@ class Node(Node2):
         """ wait for spin thread """
 
         try:
+            self._executor.wake()
             self._spin_thread.join()
         finally:
             self.get_logger().info("Destroying node " + self.get_name())
             self.destroy_node()
+
+    def create_action_client(self, action_type, action_name: str) -> ActionClient:
+        """ create action client from node
+
+        Args:
+            action_type ([type]): action type (msg action)
+            action_name (str): action name
+
+        Returns:
+            ActionClient: client created
+        """
+
+        return ActionClient(self, action_type, action_name)
+
+    def create_action_server(self,
+                             action_type,
+                             action_name,
+                             execute_callback: str,
+                             cancel_callback=None) -> ActionSingleServer:
+        """ create action server from node
+
+        Args:
+            action_type ([type]): action type (msg action)
+            action_name (str): action name
+            execute_callback ([type]): execute function
+            cancel_callback ([type], optional): cancel function. Defaults to None.
+
+        Returns:
+            ActionSingleServer: server created
+        """
+
+        return ActionSingleServer(self,
+                                  action_type,
+                                  action_name,
+                                  execute_callback,
+                                  cancel_callback=cancel_callback)
+
+    def create_action_queue_server(self,
+                                   action_type,
+                                   action_name,
+                                   execute_callback: str,
+                                   cancel_callback=None) -> ActionQueueServer:
+        """ create action queue server from node
+
+        Args:
+            action_type ([type]): action type (msg action)
+            action_name (str): action name
+            execute_callback ([type]): execute function
+            cancel_callback ([type], optional): cancel function. Defaults to None.
+
+        Returns:
+            ActionQueueServer: queue server created
+        """
+
+        return ActionQueueServer(self,
+                                 action_type,
+                                 action_name,
+                                 execute_callback,
+                                 cancel_callback=cancel_callback)
