ededdde587f873a7e4dcdae9637899e5eb9aef29
Miguel Ángel González Santamarta <mgons@unileon.es>
2021-12-06 15:04:06 +0100

    run_pytests fixed

--- merlin2_arch/merlin2_planning_layer/merlin2_pddl_generator/tests/python/run_pytests.sh
+++ merlin2_arch/merlin2_planning_layer/merlin2_pddl_generator/tests/python/run_pytests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ros2 run kant_knowledge_base knowledge_base_node &
+pid=$!
 
 pkg=merlin2_pddl_generator
 ros_pkg_path=$(python3 -c "from ament_index_python.packages import get_package_prefix; print(get_package_prefix('$pkg'))")
@@ -8,3 +9,4 @@ py_pkg_path=$(python3 -c "import os, $pkg; print(os.path.dirname($pkg.__file__))
 python3 -m pytest --cov=$py_pkg_path $ros_pkg_path/pytests/*.py
 
 pkill -9 -e -f knowledge_base_node
+wait $pid
