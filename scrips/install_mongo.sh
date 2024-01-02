2c9d0d0abf9cb46d15635fce8aa212e6ac1ab7b4
Miguel Ángel González Santamarta <mgons@unileon.es>
2024-01-02 15:42:19 +0100

    Update install_mongo.sh

--- scrips/install_mongo.sh
+++ scrips/install_mongo.sh
@@ -1,5 +1,5 @@
 wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc |  gpg --dearmor | tee /usr/share/keyrings/mongodb.gpg > /dev/null
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 apt update
-apt install mongodb-org
+apt install mongodb-org -y
 systemctl start mongod
