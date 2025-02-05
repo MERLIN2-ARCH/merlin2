79336ff9a16c17ed337f7c5679ae229b03e6f5bd
Miguel Ángel González Santamarta <mgons@unileon.es>
2025-02-05 22:52:38 +0100

    new version 2.2.0

--- merlin2_arch/merlin2_mission_layer/merlin2_mission/setup.py
+++ merlin2_arch/merlin2_mission_layer/merlin2_mission/setup.py
@@ -4,7 +4,7 @@ package_name = "merlin2_mission"
 
 setup(
     name=package_name,
-    version="0.0.0",
+    version="2.2.0",
     packages=[package_name],
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
