aa246b927502ab6fbd1e709622a11a40f7d0b431
Irene González Fernández <igonzf06@estudiantes.unileon.es>
2025-05-02 10:38:03 +0200

    fix format

--- merlin2_demos/merlin2_demos/merlin2_ready_to_start_node.py
+++ merlin2_demos/merlin2_demos/merlin2_ready_to_start_node.py
@@ -6,19 +6,19 @@ from std_msgs.msg import String
 
 
 class Merlin2ReadyToStart(Node):
-
     def __init__(self):
 
         super().__init__("merlin2_ready_to_start")
         self.publisher_ = self.create_publisher(String, "keyboard_input", 10)
         self.timer_ = self.create_timer(1.0, self.publish_keyboard_input)
-    
+
     def publish_keyboard_input(self):
         entrada = input("Key 'Enter' to start: ")
         msg = String()
         msg.data = entrada
-        self.get_logger().info(f'{entrada}')
+        self.get_logger().info(f"{entrada}")
         self.publisher_.publish(msg)
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -30,5 +30,5 @@ def main(args=None):
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
